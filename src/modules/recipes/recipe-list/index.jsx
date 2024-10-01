671e5a315e5d93397a69ed29ce28fd586996264d
Anna S. A <anna.atalay@minna.tech>
2024-10-01 23:36:11 +0300

    Add corrections for lesson36 assignment

--- src/modules/recipes/recipe-list/index.jsx
+++ src/modules/recipes/recipe-list/index.jsx
@@ -6,28 +6,22 @@ export const RecipeList = ({ onRecipeClick }) => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    fetchRecipesByIngredient("chicken_breast").then((recipes) =>
-      setRecipes(recipes.length > 0 ? recipes : []),
-    );
-  }, []);
-
-  useEffect(() => {
     const fetchRecipes = async () => {
       const recipes = await fetchRecipesByIngredient("chicken");
-      setRecipes([recipes]);
+      setRecipes(recipes);
     };
     fetchRecipes();
   }, []);
 
   return (
     <>
-      {recipes.length > 0 ? (
+      {recipes?.length > 0 ? (
         <ul className="recipe-list">
           {recipes.map((recipe) => (
             <li
-              key={recipes.idMeal}
+              key={recipe.idMeal}
               className="recipe-item"
-              onClick={() => onRecipeClick(recipes.idMeal)}
+              onClick={() => onRecipeClick(recipe.idMeal)}
             >
               {recipe.strMeal}
             </li>
