eb3e511b4a1955f6e9439df5e7f9927da2b8d2fc
Selceli <selcanece@gmail.com>
2024-10-01 12:31:43 +0300

    fetchById

--- src/modules/recipes/recipeService.js
+++ src/modules/recipes/recipeService.js
@@ -9,3 +9,15 @@ export async function fetchRecipesByIngredient(query) {
   const data = await response.json();
   return data.meals;
 };
+
+export async function fetchRecipesById(query) {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${query}`
+  );
+
+  if (!response.ok) {
+    throw Error;
+  }
+  const data = await response.json();
+  return data.meals;
+};
